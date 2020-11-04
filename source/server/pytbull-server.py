8ef9b537361c8967dbfb5ca7287eff418d7f6cb8
netrunn3r <netrunn3r@protonmail.com>
2020-11-04 17:48:29 +0100

    First changes to pytbull-server.py
    1. Migrate to Python 3
    2. Rename to pytbull-ng
    3. Create infinite loop to accept connections from many runs of attacker container

--- source/server/pytbull-server.py
+++ source/server/pytbull-server.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 """
-Pytbull server script (reverse shell)
-Used for client side attacks
+Pytbull-ng server script (reverse shell)
+Used for client side attacks and as a receiver of all attacks
 
-Pytbull is an IDS/IPS testing framework for Snort & Suricata and any IDS/IPS
-provided you can grab the alerts file via FTP.
-It is developed by Sebastien Damaye (sebastien #dot# damaye #at# gmail #dot# com).
+Pytbull-ng is an IDS/IPS testing framework for any IDS/IPS.
+Original version, pytbull, was developed by Sebastien Damaye (sebastien #dot# 
+damaye #at# gmail #dot# com). Michal Chrobak continue development of this tool
+as pytbull-ng, making it more adapted to the current days.
+
 It is shipped with 300 tests grouped in 9 testing modules. For more information,
-please refer to <http://www.aldeid.com/index.php/Pytbull>.
+please refer to <http://www.github.com/netrunn3r/pytbull-ng>.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -35,58 +37,64 @@ class PytbullServer():
         self.port = port
         self.socksize = 1024
 
-        print banner
-        print ""
+        print(banner)
+        print("")
 
         # Checking root privs
-        print "Checking root privileges".ljust(65, '.'),
+        print("Checking root privileges".ljust(65, '.'), end='')
         if(os.getuid() != 0):
-            print "[ Failed ]"
-            print "\n***Error: Root privileges required!"
+            print("[ Failed ]")
+            print("\n***Error: Root privileges required!")
             sys.exit(0)
-        print "[   OK   ]"
+        print("[   OK   ]")
 
     def reverseShell(self):
-        print "Checking port to use".ljust(65, '.'),
+        print("Checking port to use".ljust(65, '.'), end='')
         try:
             # Open a socket on localhost, port 12345/tcp by default
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.bind((self.host, int(self.port)))
-            print "[   OK   ]"
-        except Exception, err:
-            print "[ Failed ]"
-            print "\n***ERROR: %s" % err
+            print("[   OK   ]")
+        except Exception as err:
+            print("[ Failed ]")
+            print("\n***ERROR: %s" % err)
             sys.exit(0)
 
-        print ""
-        print "Server started on port: %s" % self.port
+        print("")
+        print("Server started on port: %s" % self.port)
         s.listen(1)
-        print "Listening..."
-        conn, addr = s.accept()
         while True:
-            print 'New connection from %s:%d' % (addr[0], addr[1])
-            data = conn.recv(self.socksize)
-            cmd = ['/bin/sh', '-c', data]
-            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE).wait()
-            # Issue 3450032 - Synchronisation issue. The server has to instruct
-            # the client that the file has been successfully downloaded before
-            # it goes to next file
-            conn.send('done')
-            if not data:
-                break
-            elif data == 'killsrv':
-                sys.exit()
+            print("Listening...")
+            try:
+                conn, addr = s.accept()
+                while True:
+                    print('\n> Orders from %s:%d' % (addr[0], addr[1]))
+                    data = conn.recv(self.socksize).decode()
+                    print(f'> Command to execute: {data}')
+                    cmd = ['/bin/sh', '-c', data]
+                    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE).wait()
+                    # Issue 3450032 - Synchronisation issue. The server has to instruct
+                    # the client that the file has been successfully downloaded before
+                    # it goes to next file
+                    conn.send('done'.encode())
+                    if not data:
+                        break
+                    elif data == 'killsrv':
+                        sys.exit()
+            except ConnectionResetError as e:
+                print('Connection reset by peer, starting new listener')
 
 if __name__ == '__main__':
 
     banner = """
-                                 _   _           _ _
-                     _ __  _   _| |_| |__  _   _| | |
-                    | '_ \| | | | __| '_ \| | | | | |
-                    | |_) | |_| | |_| |_) | |_| | | |
-                    | .__/ \__, |\__|_.__/ \__,_|_|_|
-                    |_|    |___/
-                       Sebastien Damaye, aldeid.com"""
+                           __  __          ____                 
+              ____  __  __/ /_/ /_  __  __/ / /     ____  ____ _
+             / __ \/ / / / __/ __ \/ / / / / /_____/ __ \/ __ `/
+            / /_/ / /_/ / /_/ /_/ / /_/ / / /_____/ / / / /_/ / 
+           / .___/\__, /\__/_.___/\__,_/_/_/     /_/ /_/\__, /  
+          /_/    /____/                                /____/   
+           creator of pytbull:    Sebastien Damaye, aldeid.com
+           creator of pytbull-ng: Michal Chrobak,   efigo.pl"""
     parser = OptionParser(usage="sudo ./%prog [-p port]")
     parser.add_option("-p", "--port", dest="port", default=12345,
         help="tcp port to open for reverse shell (default to 12345)")
